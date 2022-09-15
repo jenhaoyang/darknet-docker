d0f9e7c6bb15a5ef947c40a19cdec0ebb67a70d3
Steven <2005013@iisigroup.com>
2022-09-15 18:17:55 +0800

    更新docker

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 for var in cpu cpu-noopt gpu gpu-cc53 gpu-cc60 gpu-cc61 gpu-cc62 gpu-cc70 gpu-cc72 gpu-cc75 gpu-cc80 gpu-cc86 \
           cpu-cv cpu-noopt-cv gpu-cv gpu-cv-cc53 gpu-cv-cc60 gpu-cv-cc61 gpu-cv-cc62 gpu-cv-cc70 gpu-cv-cc72 gpu-cv-cc75 gpu-cv-cc80 gpu-cv-cc86
 do
-  DOCKER_REPO="daisukekobayashi/darknet"
+  DOCKER_REPO="darknet-docker"
   SOURCE_BRANCH="master"
   SOURCE_COMMIT=`git ls-remote https://github.com/AlexeyAB/darknet.git ${SOURCE_BRANCH} | awk '{ print $1 }'`
   DOCKER_TAG=$var
